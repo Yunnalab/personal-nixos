50a929416a145214b8ffe5d7663af608d9bb9753
acloudygirl <283527013@qq.com>
2026-09-07 14:08:34 +0800

    退回nixpkgs,因为更新的ssdm包会导致登录界面键盘输入失效

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "NixOS configuration for nixos";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # 固定到 Qt 6.11.1 的 commit（避免 Qt 6.11.2 导致 SDDM Wayland greeter 键盘失灵的回归）。
+    # 待 Qt 6.11.3 / nixpkgs 修复后可恢复为 nixos-unstable。
+    nixpkgs.url = "github:NixOS/nixpkgs/0e251e24a4f24e036a084b6b4b2d2491af4167f4";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
